9d724e071efdb686da7f1f9dba33185f36b0bd68
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2022-03-09 21:23:07 -0500

    feat (PokemonTable): Componente creado y con pruebas de existencia

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { CrudPanel } from "./CrudPanel";
+import { PokemonTable } from "./PokemonTable";
 
 export const MainPage = () => {
 	return (
 		<>
 			<h1>Listado de Pokemon</h1>
 			<CrudPanel />
+			<PokemonTable />
 		</>
 	);
 };
---
+++ src/components/PokemonTable.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const PokemonTable = () => {
+	return (
+		<table role="pokemon-table">
+			<thead>
+				<tr>
+					<th>Nombre</th>
+					<th>Imagen</th>
+					<th>Ataque</th>
+					<th>Defensa</th>
+					<th>Acciones</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>Test</td>
+					<td>Test</td>
+					<td>Test</td>
+					<td>Test</td>
+					<td>Test</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+};

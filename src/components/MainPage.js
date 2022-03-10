fa4c81a2920012529fc1f264b82bc30e61d0048d
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2022-03-09 21:32:37 -0500

    feat (PokemonEdit): Componente creado y pasando test de existencia

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { CrudPanel } from "./CrudPanel";
+import { PokemonEdit } from "./PokemonEdit";
 import { PokemonTable } from "./PokemonTable";
 
 export const MainPage = () => {
@@ -8,6 +9,7 @@ export const MainPage = () => {
 			<h1>Listado de Pokemon</h1>
 			<CrudPanel />
 			<PokemonTable />
+			<PokemonEdit />
 		</>
 	);
 };
---
+++ src/components/PokemonEdit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const PokemonEdit = () => {
+	return (
+		<div role="pokemon-edit">
+			<h2>Nuevo Pokemon</h2>
+			<form>
+				<div>
+					<div></div>
+				</div>
+			</form>
+		</div>
+	);
+};

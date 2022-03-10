cb1e20604e0e85a6a015462fb4aad1139767868d
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2022-03-10 12:26:22 -0500

    test(Pokemon Table): Test que prueba que los pokemones que aparecen en la lista sean los solicitados

--- src/__tests__/PokemonTable.test.js
+++ src/__tests__/PokemonTable.test.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { render, screen, fireEvent, getByRole } from "@testing-library/react";
+import {
+	render,
+	screen,
+	fireEvent,
+	getByRole,
+	waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { MainPage } from "../components/MainPage";
 
 //Implementación de un mock server para probar los getRequest en una tabla
 
-beforeAll(() => render(<MainPage />));
-
 const server = setupServer(
 	rest.get("/pokemons", (req, res, ctx) => {
-		const pokemonName = req.url.searchParams.get("name");
+		const pokeName = req.url.searchParams.get("name");
 		return res(
 			ctx.json([
 				{
 					id: 1013,
-					name: pokemonName,
+					name: pokeName,
 					image:
 						"https://terrigen-cdn-dev.marvel.com/content/prod/1x/017lok_ons_crd_03.jpg",
 					type: "water",
@@ -27,8 +32,8 @@ const server = setupServer(
 					updated_at: "2022-03-09T20:35:29.430Z",
 				},
 				{
-					id: 1013,
-					name: pokemonName,
+					id: 1014,
+					name: pokeName,
 					image:
 						"https://terrigen-cdn-dev.marvel.com/content/prod/1x/017lok_ons_crd_03.jpg",
 					type: "water",
@@ -47,10 +52,29 @@ const server = setupServer(
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 
+//Setup para cambiar el valor de la caja de búsqueda
+
+const setupNameSearcher = () => {
+	let utils = null;
+	act(() => {
+		utils = render(<MainPage />);
+	});
+	const input = utils.getByRole("search");
+	return {
+		input,
+		...utils,
+	};
+};
+
 describe("Mostrar pokemones por nombre", () => {
-	it("debe mostrar los pokemones de acuerdo al nombre que se consulte", () => {
+	it("debe mostrar los pokemones de acuerdo al nombre que se consulte", async () => {
+		const { input } = setupNameSearcher();
+		act(() => {
+			fireEvent.change(input, { target: { value: "Pikachu" } });
+		});
+		await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
 		const [firstPoke, secondPoke] = screen.getAllByRole("pokemon-name");
-		expect(firstPoke.textContent).toBe(screen.getByRole("search").value);
-		expect(secondPoke.textContent).toBe(screen.getByRole("search").value);
+		expect(firstPoke.textContent).toBe(input.value);
+		expect(secondPoke.textContent).toBe(input.value);
 	});
 });

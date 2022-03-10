2c292645415f094d8cbec2b2473bc8d424b996d9
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2022-03-09 21:14:42 -0500

    feat (CrudPanel): Agregado el componente CrudPanel

---
+++ src/components/CrudPanel.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const CrudPanel = () => {
+	return (
+		<div>
+			<div>
+				<input type="text" role="search" />
+			</div>
+
+			<div>
+				<button role="create">Crear</button>
+			</div>
+		</div>
+	);
+};
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { CrudPanel } from "./CrudPanel";
 
 export const MainPage = () => {
 	return (
 		<>
 			<h1>Listado de Pokemon</h1>
+			<CrudPanel />
 		</>
 	);
 };
